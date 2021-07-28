fb95426e8c0b3e202919426fc109e7d09504e145
Gino Villalpando <gino@rs21.io>
2021-07-28 13:19:25 -0600

    (Bug Fix): fixed values not updating after adding new button

--- viz-lib/src/visualizations/selection-table/Editor/ButtonSettings.tsx
+++ viz-lib/src/visualizations/selection-table/Editor/ButtonSettings.tsx
@@ -5,21 +5,30 @@ import { Section, Input, Checkbox } from "@/components/visualizations/editor";
 import { EditorPropTypes } from "@/visualizations/prop-types";
 import Button from "antd/lib/button";
 import { useState, useEffect } from "react";
+import Item from "antd/lib/list/Item";
 
 type Props = {
   options: {
-    buttonName: string;
-    buttonLinkUrlTemplate?: string;
-    buttonLinkOpenInNewTab?: boolean;
+    name: string;
+    linkUrlTemplate?: string;
+    linkOpenInNewTab?: boolean;
   };
   onChange: (...args: any[]) => any;
 };
 
-export default function ButtonSettings({ onChange }: Props) {
-  const [arr, setArr]: any[] = useState([]);
-  function handleChange(item: any, checked: any) {
-    item.linkOpenInNewTab = checked;
-  }
+const ButtonSettings = ({ onChange }: Props) => {
+  const [arr, setArr]: any[] = useState([{ name: "", linkUrlTemplate: "", linkOpenInNewTab: true }]);
+  const handleChange = (event: any, item: any) => {
+    console.log(event.target, item);
+    if (event.target.id === "Button-Name") {
+      item.name = event.target.value;
+    } else if (event.target.id === "URL-Template") {
+      item.linkUrlTemplate = event.target.value;
+    } else if (event.target.type === "checkbox") {
+      item.linkOpenInNewTab = event.target.checked;
+    }
+    setArr((prevArr: any) => [...prevArr]);
+  };
   const [onChangeDebounced] = useDebouncedCallback(onChange, 200);
   const [test, setTest]: any[] = useState([]);
   const [options, setOptions]: any[] = useState([]);
@@ -30,61 +39,52 @@ export default function ButtonSettings({ onChange }: Props) {
     console.log("arr", arr);
   };
 
-  useEffect(() => {
-    const newArr: any = [];
-
-    arr.forEach((item: any, index: any) => {
-      const newItem = (
-        <React.Fragment key={index}>
-          {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
-          <Section key={index} item={item}>
-            <Input
-              label="Button Name"
-              // data-test="Table.ColumnEditor.Link.Name"
-              defaultValue={item.name}
-              onChange={(event: any) => onChangeDebounced({ buttonName: event.target.value })}
-            />
-          </Section>
-          {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
-          <Section>
-            <Input
-              label="URL template"
-              // data-test="Table.ColumnEditor.Link.UrlTemplate"
-              defaultValue={item.linkUrlTemplate}
-              onChange={(event: any) => onChangeDebounced({ buttonLinkUrlTemplate: event.target.value })}
-            />
-          </Section>
-
-          {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
-          <Section>
-            <Checkbox
-              // data-test="Table.ColumnEditor.Link.OpenInNewTab"
-              value={item.linkOpenInNewTab}
-              checked={item.linkOpenInNewTab}
-              onChange={event => {
-                item.linkOpenInNewTab = event.target.checked;
-                console.log(item.linkOpenInNewTab);
-                return item.linkOpenInNewTab;
-              }}>
-              Open in new tab
-            </Checkbox>
-          </Section>
-        </React.Fragment>
-      );
-      newArr.push(newItem);
-    });
-
-    setTest(newArr);
-  }, [arr]);
-
   return (
     <>
       <Button onClick={doSomething}>Do Something</Button>
-      {test}
+      {map(arr, (item: any, index: any) => {
+        return (
+          <React.Fragment key={index}>
+            {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
+            <Section key={index} item={item}>
+              <Input
+                label="Button Name"
+                id="Button-Name"
+                // data-test="Table.ColumnEditor.Link.Name"
+                defaultValue={item.name}
+                onChange={(event: any) => handleChange(event, item)}
+              />
+            </Section>
+            {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
+            <Section>
+              <Input
+                label="URL template"
+                id="URL-Template"
+                // data-test="Table.ColumnEditor.Link.UrlTemplate"
+                defaultValue={item.linkUrlTemplate}
+                onChange={(event: any) => handleChange(event, item)}
+              />
+            </Section>
+
+            {/* @ts-expect-error ts-migrate(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
+            <Section>
+              <Checkbox
+                // data-test="Table.ColumnEditor.Link.OpenInNewTab"
+                value={item.linkOpenInNewTab}
+                checked={item.linkOpenInNewTab}
+                onChange={event => handleChange(event, item)}>
+                Open in new tab
+              </Checkbox>
+            </Section>
+          </React.Fragment>
+        );
+      })}
     </>
   );
-}
+};
 
 ButtonSettings.defaultProps = {
   onChange: () => {},
 };
+
+export default ButtonSettings;
